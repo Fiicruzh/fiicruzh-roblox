f7395a961e8017dd66a1055f72b4b84072f41e99
Fiicruzh <ownersfii@gmail.com>
2026-04-05 17:03:06 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const USER_ID = 8941948601;
+
+app.get("/api", async (req,res)=>{
+  try{
+    const [a,b,c] = await Promise.all([
+      fetch(`https://friends.roblox.com/v1/users/${USER_ID}/friends/count`).then(r=>r.json()),
+      fetch(`https://friends.roblox.com/v1/users/${USER_ID}/followers/count`).then(r=>r.json()),
+      fetch(`https://friends.roblox.com/v1/users/${USER_ID}/followings/count`).then(r=>r.json())
+    ]);
+
+    res.json({
+      friends:a.count,
+      followers:b.count,
+      following:c.count
+    });
+
+  }catch{
+    res.json({friends:0, followers:0, following:0});
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server jalan 💀"));
